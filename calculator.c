1a3304007ec99248b2664a096ae24039ad5ed14d
GopalDevda <devdagopal54@gmail.com>
2022-07-23 14:46:40 +0530

    Initial commit

---
+++ calculator.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main(){
+	int a , b;
+    printf("Enter two numbers \n");
+	scanf("%d%d",&a,&b);
+	printf("Addition of the numbers is %d\n",a+b);
+	printf("Substraction of the numbers is %d\n",a-b);
+	printf("Multiplication of the numbers is %d\n",a*b);
+	printf("Division of the numbers is %f\n",a/b);
+    return 0;
+
+}
